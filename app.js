8eeb2c2c94155f0550d738e0a8799aef0d2060be
regulo <regulo.leonardi@gmail.com>
2017-05-07 17:49:03 +0200

    package json

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.use('/rest', api);
 
 // Start server
 app.listen(3000, function() {
-    console.log("Node server running on http://localhost:3000");
+    console.log("Node server running on port 3000");
 });
 
